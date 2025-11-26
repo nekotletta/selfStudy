9a914d8f904752ccc7b955ee91e2d8a72f03be92
nekotletta <forprogrammingpurposes02@gmail.com>
2025-11-25 21:21:12 -0400

    added scientific notation support + exp

--- scheme/calculator.scm
+++ scheme/calculator.scm
@@ -7,27 +7,63 @@
     (number? x)
 )
 
-(define (add exp1 exp2 curr-ans)
-    (+ (determine-action exp1 curr-ans) (determine-action exp2 curr-ans))     
+(define (compute exp1 exp2 curr-ans op)
+    (op (determine-action exp1 curr-ans) (determine-action exp2 curr-ans))
 )
 
-(define (sub exp1 exp2 curr-ans)
-    (- (determine-action exp1 curr-ans) (determine-action exp2 curr-ans)) 
+
+
+(define (scientific-repr num)
+    (define (truncate-one-place n)
+        (/ (truncate (* n 10.0)) 10.0)
+    )
+    (let ((e (floor (/ (log num) (log 10)))))
+        (define dec (/ num (power 10 e 1.0)))
+        (define d (truncate-one-place dec))
+        (string-append (number->string d) "E" (number->string e))
+    )
+)
+
+(define (scientific-notation b e curr-ans)
+    (let ((base (determine-action b curr-ans))
+          (exponent (determine-action e curr-ans)))
+        (* base (power 10 exponent 1.0))
+    )
+)
+
+
+; supports negative exponents, somewhat
+(define (power pow num curr-ans)
+    (letrec ((power-helper
+        (lambda (base exponent)
+            (cond ((= exponent 0) 1.0)
+                   ((= exponent 1) base)
+                   (( = base 1) 1.0)
+                   ((> exponent 0) (* base (power-helper base (- exponent 1))))
+                   ((< exponent 0) (/ 1 (power-helper base (* -1 exponent))))
+            )
+        )))
+        (power-helper (determine-action pow curr-ans) (determine-action num curr-ans))
+    )
 )
 
  (define (determine-action exp curr-ans)
-    (cond ((is-number exp) exp)
+    (cond ((is-number exp) (exact->inexact exp))
         ((and (symbol? exp) (eq? exp 'ans)) curr-ans)
         ((pair? exp)
             (let ((first-exp (cadr exp))
             (second-exp (caddr exp)))
             (cond 
-                ((eq? (car exp) '+) (add first-exp second-exp curr-ans))
-                ((eq? (car exp) '-) (sub first-exp second-exp curr-ans))
+                ((eq? (car exp) '+) (compute first-exp second-exp curr-ans + ))
+                ((eq? (car exp) '-) (compute first-exp second-exp curr-ans -))
+                ((eq? (car exp) '*) (compute first-exp second-exp curr-ans * ))
+                ((eq? (car exp) '/) (compute first-exp second-exp curr-ans /))
+                ((eq? (car exp) '^) (power first-exp second-exp curr-ans))
+                ((eq? (car exp) 'E) (scientific-notation first-exp second-exp curr-ans))
                 (else (error "Unknown operation"))
             ))
-        ) 
-    )
+        ) (else (error "Unknown expression"))
+    ) 
  )
 
 
@@ -44,10 +80,22 @@
             (newline))
         (begin
              (let ((result (compile input-exp ans)))
-                (display "Result: ")
-                (display result)(newline)
+                (if (or (> result 100000) (< result 0.00001))
+                    (begin
+                        (display "Result in scientific notation: ")
+                        (display (scientific-repr result))
+                        (newline))
+                 (begin
+                    (display "Result: ")
+                    (display result)
+                    (newline)
+                ))
+        
 
              (input-loop result))))
 )
 
+(display "Welcome to the Scheme Calculator!")(newline)
+(display "Plase enter expressions in polish notation. (+ 2 4)")(newline)
+(display "Supports: arithmethic operations, power (^), scientific notation (E), and ans")(newline)(newline)
 (input-loop 0)
