cd9cf4188369a1f62a6c7caece142399ff737158
nekotletta <forprogrammingpurposes02@gmail.com>
2025-11-27 00:00:06 -0400

    calculator supports vars / is done

--- scheme/calculator.scm
+++ scheme/calculator.scm
@@ -1,8 +1,8 @@
 #lang scheme
 
-; simple calculator that handles addition and substraction
-; along with the special variable 'ans' that holds the last answer
-
+; calculator built on scheme with support for:
+; arithmethics, exponentiation, scientific notation,
+; ans keyboard, and variable definitions
 (define (is-number x)
     (number? x)
 )
@@ -22,17 +22,68 @@
 )
 
 
-(define (var-defined all-vars var)
+(define (var-defined var all-vars)
     (fold (lambda (rest curr) (if (eq? (car curr) var) #t rest)) #f all-vars)
 )
 
-(define (def var)
-    (display "hihi")
+(define (get-var-value var all-vars)
+    (if (var-defined var all-vars)
+        (fold (lambda (rest curr) (if (eq? (car curr) var) (cadr curr) rest)) #f all-vars)
+        #f
+    )
+)
+
+(define (update-var var new-val all-vars)
+  (cond
+    ((null? all-vars) '()) 
+    ((eq? (car (car all-vars)) var) (cons (list var new-val) (cdr all-vars)))  
+    (else (cons (car all-vars) (update-var var new-val (cdr all-vars))))
+)) 
+
+(define (def new-var new-val curr-ans all-vars)
+    (let (( var (determine-action new-var curr-ans all-vars))
+          ( val (determine-action new-val curr-ans all-vars)))
+
+        (if (var-defined var all-vars)
+
+            (let ((updated-pair (update-var var val all-vars)))
+                (list curr-ans updated-pair))
+
+            (let ((new-pair (cons (list var val) all-vars)))
+                (list curr-ans new-pair))
+        )
+
+    )
 )
 
 (define (compute exp1 exp2 curr-ans op vars)
-    (let (( result (op (determine-action exp1 curr-ans vars) (determine-action exp2 curr-ans vars))))
-    (list result vars))
+
+    (let ((val1 (determine-action exp1 curr-ans vars))
+        (val2 (determine-action exp2 curr-ans vars)))
+
+        ; compute returns a list of result and vars
+        ; we can operate of the result of the computation
+        (when (list? val1)
+            (set! val1 (car val1)))
+        (when (list? val2)
+            (set! val2 (car val2)))
+
+        ; check if either of these values is a variable
+        ; if it is, validate that the variable exists
+        (when (and (symbol? val1) (not (eq? val1 'ans)))
+            (if (var-defined val1 vars)
+                (set! val1 (get-var-value val1 vars))
+            (error (string-append (symbol->string val1) ": Undefined variable"))))
+
+        (when (and (symbol? val2) (not (eq? val2 'ans)))
+            (if (var-defined val2 vars)
+                (set! val2 (get-var-value val2 vars))
+            (error (string-append (symbol->string val2) ": Undefined variable"))))
+
+
+        (let ((result (op val1 val2)))
+            (list result vars))
+    )
 )
 
 (define (scientific-repr num)
@@ -72,6 +123,7 @@
  (define (determine-action exp curr-ans curr-vars)
     (cond ((is-number exp) (exact->inexact exp))
         ((and (symbol? exp) (eq? exp 'ans)) curr-ans)
+        ((and (symbol? exp) (not (eq? exp 'ans))) exp)
         ((pair? exp)
             (let ((first-exp (cadr exp))
             (second-exp (caddr exp)))
@@ -82,6 +134,7 @@
                 ((eq? (car exp) '/) (compute first-exp second-exp curr-ans / curr-vars))
                 ((eq? (car exp) '^) (power first-exp second-exp curr-ans))
                 ((eq? (car exp) 'E) (scientific-notation first-exp second-exp curr-ans))
+                ((eq? (car exp) 'DEF) (def first-exp second-exp curr-ans curr-vars))
                 (else (error "Unknown operation"))
             ))
         ) (else (error "Unknown expression"))
@@ -108,21 +161,27 @@
                         (display (scientific-repr (car result)))
                         (newline)
                     )
+                    (if (not (= (car result) 0))
                     (begin
                         (display "Result: ")
                         (display (car result))
                         (newline)
-                    )
+                    ) (newline))
                 )
+                (if (not (null? (cadr result)))
+                (begin
+                    (display "Your current vars are:")(newline)
+                    (list-vars (cadr result))(newline)
+                ) (newline))
 
-                (display "Your current vars are:")(newline)
-                (list-vars (cadr result))(newline)
+
+                
         
 
-             (input-loop result (cadr result)))))
+             (input-loop (car result) (cadr result)))))
 )
 
 (display "Welcome to the Scheme Calculator!")(newline)
 (display "Plase enter expressions in polish notation. (+ 2 4)")(newline)
-(display "Supports: arithmethic operations, power (^), scientific notation (E), and ans")(newline)(newline)
-(input-loop 0 '((x 12) (y 3)))
+(display "Supports: arithmethic operations, power (^), scientific notation (E), variables (DEF), and ans")(newline)(newline)
+(input-loop 0 '())
